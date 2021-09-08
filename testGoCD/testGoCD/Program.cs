aa45bd1c0eabae20e73fde5d484e2db881109a17
yanyuxin003 <yanyuxin003@163.com>
2021-09-08 20:07:57 +0800

    test gocd

---
+++ testGoCD/testGoCD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testGoCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test GoCD!");
+        }
+    }
+}
